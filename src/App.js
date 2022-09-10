4ebac07ad4f459817922b69c531e0162ceaeaa12
Patrick Sontag <pmsontag84@gmail.com>
2022-09-10 11:07:10 -0500

    card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import sampleCard from './sampleCard'
+import Card from './Card.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Card cardCode="2S" />
+        <Card cardCode="8C" />
+        <Card cardCode="0H" />
+        <Card cardCode="QD" />
+        <Card cardCode="2S" />
+        <Card cardCode="9H" />
+        <Card cardCode="JC" />
+      </div>
     </div>
   );
 }
