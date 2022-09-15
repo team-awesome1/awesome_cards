5d5c2bff58c2c2d21bd184176d39cbcfb4e24507
Jose_Fernandez <jfernand84@gmail.com>
2022-09-14 20:37:27 -0400

    feature/homepage-sync_routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <nav
-        style={{
-          borderBottom: 'solid 1px',
-          paddingBottom: '1rem',
-        }}>
-        <Link to='/' id="home-link">Home</Link> | <Link to='/instructions' id="instructions-link">Instructions</Link> | <Link to='/about' id="about-link">About</Link>
-      </nav>
+      {/* a nav bar can safely go here */}
       <Outlet />
     </div>
   );
