a89d6d58417b5070fdb01d7970da99fb5eb6cbf5
Jose_Fernandez <jfernand84@gmail.com>
2022-09-18 13:12:04 -0400

    bulma setup with project

---
+++ src/routes/TestPage.js
@@ -0,0 +1,13 @@
+function TestPage() {
+  return (
+    <div className='TestPage'>
+      <section class='hero is-link is-fullheight-with-navbar'>
+        <div class='hero-body'>
+          <p class='title'>Fullheight hero with navbar</p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default TestPage;
