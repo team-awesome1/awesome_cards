eb99e692d3f0d705f2ac387c795ef8190d7a0a78
Tina Flower <tmflower@gmail.com>
2022-09-18 18:03:55 -1000

    created navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,12 +9,10 @@ import SignIn from './routes/SignIn';
 
 
 const Navbar = () => {
-    const curr_location = window.location.pathname;
-    console.log(curr_location);
     return (
         <nav className="Navbar">
             <ul className="NavLinks">
-            <NavLink to="/" element={<Homepage />}><li>Home</li></NavLink> {/* "Sign In" should change to "Sign Out" if user is already logged in */}
+            <NavLink to="/" element={<Homepage />}><li>Home</li></NavLink>
             <NavLink to="/play-now" element={<PlayNow />}><li>Play Now</li></NavLink>
             <NavLink to="/instructions" element={<Instructions />}><li>Instructions</li></NavLink>
             <NavLink to="/about" element={<About />}><li>About</li></NavLink>
