e290741294b48566851758d7e7d1556c4866b1df
Tina Flower <tmflower@gmail.com>
2022-09-30 12:28:52 -1000

    NavBar Component created

---
+++ src/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Logo.css";
+
+const Logo = () => {
+    return (
+        <div>
+            <h1>WAR!</h1>
+        </div>
+    )
+}
+
+export default Logo;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Navbar.css';
 import Homepage from './Homepage';
 import Instructions from './routes/Instructions';
 import About from './routes/About';
 import PlayNow from './routes/PlayNow';
 import SignIn from './routes/SignIn';
+import Logo from './Logo';
+import './NavBar.css';
 
 
-const Navbar = () => {
+const NavBar = () => { 
     return (
-        <nav className="Navbar">
-            <ul className="NavLinks">
-            <NavLink to="/" element={<Homepage />}><li>Home</li></NavLink>
-            <NavLink to="/play-now" element={<PlayNow />}><li>Play Now</li></NavLink>
-            <NavLink to="/instructions" element={<Instructions />}><li>Instructions</li></NavLink>
-            <NavLink to="/about" element={<About />}><li>About</li></NavLink>
-            <NavLink to="/sign-in" element={<SignIn />}><li>Sign In</li></NavLink> {/* "Sign In" should change to "Sign Out" if user is already logged in */}
-            </ul>
+        <nav className="navbar navbar-expand-lg" id="navbar">
+            <div class="container-fluid">
+                <a class="navbar-brand pt-3"  href="/"><NavLink to="/" element={<Homepage />} id="logo"><Logo /></NavLink> </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item pt-2">
+                            <a class="nav-link active" aria-current="page" href="/play-now" ><NavLink to="/play-now" element={<PlayNow />} id="play-now">Play Now</NavLink></a>
+                        </li>
+                        <li class="nav-item pt-2">
+                            <a class="nav-link active" aria-current="page"href="/instructions" ><NavLink to="/instructions" element={<Instructions />} id="instructions">Instructions</NavLink></a>
+                        </li>
+                        <li class="nav-item pt-2">
+                            <a class="nav-link active" aria-current="page"href="/about"><NavLink to="/about" element={<About />} id="about">About</NavLink></a>
+                        </li>
+                    </ul>
+                    <button class="btn btn-outline" type="submit" id="loginBtn"><NavLink to="/sign-in" element={<SignIn />} id="loginBtnText">Sign In</NavLink></button>
+                </div>
+            </div>
         </nav>
     )
 }
 
-export default Navbar;
+export default NavBar;
