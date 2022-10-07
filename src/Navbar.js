641ecfa58caa340a859d6c0f96278b7f2e55db4e
Tina Flower <tmflower@gmail.com>
2022-10-06 15:29:26 -1000

    add navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,24 +12,24 @@ import './Navbar.css';
 const NavBar = () => { 
     return (
         <nav className="navbar navbar-expand-lg" id="navbar">
-            <div class="container-fluid">
-                <a class="navbar-brand pt-3"  href="/"><NavLink to="/" element={<Homepage />} id="logo"><Logo /></NavLink> </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <div className="container-fluid">
+                <NavLink to="/" element={<Homepage />} id="logo" className="navbar-brand pt-3"  href="/"><Logo /></NavLink> 
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item pt-2">
-                            <a class="nav-link active" aria-current="page" href="/play-now" ><NavLink to="/play-now" element={<PlayNow />} id="play-now">Play Now</NavLink></a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item pt-2">
+                            <NavLink to="/play-now" element={<PlayNow />} id="play-now" className="nav-link active" aria-current="page" href="/play-now">Play Now</NavLink>
                         </li>
-                        <li class="nav-item pt-2">
-                            <a class="nav-link active" aria-current="page"href="/instructions" ><NavLink to="/instructions" element={<Instructions />} id="instructions">Instructions</NavLink></a>
+                        <li className="nav-item pt-2">
+                            <NavLink to="/instructions" element={<Instructions />} id="instructions"className="nav-link active" aria-current="page"href="/instructions">Instructions</NavLink>
                         </li>
-                        <li class="nav-item pt-2">
-                            <a class="nav-link active" aria-current="page"href="/about"><NavLink to="/about" element={<About />} id="about">About</NavLink></a>
+                        <li className="nav-item pt-2">
+                           <NavLink to="/about" element={<About />} id="about" className="nav-link active" aria-current="page"href="/about">About</NavLink>
                         </li>
                     </ul>
-                    <button class="btn btn-outline" type="submit" id="loginBtn"><NavLink to="/sign-in" element={<SignIn />} id="loginBtnText">Sign In</NavLink></button>
+                    <button className="btn btn-outline" type="submit" id="loginBtn"><NavLink to="/sign-in" element={<SignIn />} id="loginBtnText">Sign In</NavLink></button>
                 </div>
             </div>
         </nav>
