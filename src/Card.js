9f05d6727236822289296d5a1f96c56df1babbe1
Patrick Sontag <pmsontag84@gmail.com>
2022-10-15 10:20:39 -0500

    get suit from suitCode

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import blank_card from './svg/blank_card.svg';
 import IMAGES from './images.js';
 import './styles.css';
 
@@ -11,15 +10,18 @@ const Card = ({ cardCode }) => {
 
     console.log("cardCode:", cardCode)
     let value = cardCode[0]
-    let suit = cardCode[1]
+    let suitCode = cardCode[1]
+    let suit;
     let valueColor;
     console.log("value", value)
-    console.log("suit", suit)
-    if (suit === "H" || suit === "D") {
+    console.log("suit", suitCode)
+    if (suitCode === "H" || suitCode === "D") {
         valueColor = "suit-heart-diamond";
+        suit = suitCode === "H" ? "heart" : "diamond";
     } else {
         valueColor = "suit-club-spade";
-    }
+        suit = suitCode === "C" ? "club" : "spade";
+    }   
     if (value === "0") {value = "10"};
 
     return (
@@ -30,7 +32,7 @@ const Card = ({ cardCode }) => {
                     <div class="card-background">
                         <div class={`card-value-top ${valueColor}`}>{value}</div>
                         <div class={`card-value-bottom ${valueColor}`}>{value}</div>
-                        <img className="card-suit" src={IMAGES[suit]} alt="card suit" />
+                        <img className="card-suit" src={IMAGES[suitCode]} alt="card suit" />
                     </div>
                 </div>
             </div>
