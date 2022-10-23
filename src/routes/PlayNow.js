a5b7bd2668811045cd681059d3b9cec023db6fb9
Tina Flower <tmflower@gmail.com>
2022-10-22 14:54:28 -1000

    adds scoreboard component

--- src/routes/PlayNow.js
+++ src/routes/PlayNow.js
@@ -1,6 +1,7 @@
 import Deck from "../deckData";
 import Player from '../Player';
 import { useState } from "react";
+import Scoreboard from "../scoreboard/Scoreboard";
 
 export default function PlayNow() {
   const [hand1Data, setHand1Data] = useState(null);
@@ -19,7 +20,8 @@ export default function PlayNow() {
       <h3>Player 1 Hand</h3>
       {hand1Data ? (<Player handData={hand1Data} />) : (<></>)}
       <h3>Player 2 Hand</h3>
-      {hand2Data ? (<Player handData={hand2Data} />) : (<></>)}
+      {hand2Data ? (<Player handData={hand2Data} />) : (<></>)}      
+      <Scoreboard />
     </>
   );
 }
