c4f5987c4c0747e0597d099f48e1d51137099ce7
Jose_Fernandez <jfernand84@gmail.com>
2022-10-28 14:48:06 -0400

    optional background / dynamic cardArea

--- src/CardArea.js
+++ src/CardArea.js
@@ -2,22 +2,24 @@ import Player from './Player';
 import './styles/CardArea.scss';
 
 export default function CardArea(props) {
-  const { hand1Data, hand2Data } = props;
+  const { hand1Data, hand2Data, gameInProgress } = props;
 
   return (
     <div className='cardArea-wrapper'>
-      <div className='hero is-success is-small'>
-        <div className='card_area'>
-          <div className='container'>
-            <h3>Player 1 Hand</h3>
-            {hand1Data ? <Player handData={hand1Data} /> : <></>}
-          </div>
-          <div className='container'>
-            <h3>Player 2 Hand</h3>
-            {hand2Data ? <Player handData={hand2Data} /> : <></>}
+      {gameInProgress ? (
+        <div className='hero is-success is-small'>
+          <div className='card_area'>
+            <div className='container'>
+              <h3>Player 1 Hand</h3>
+              {hand1Data ? <Player handData={hand1Data} /> : <></>}
+            </div>
+            <div className='container'>
+              <h3>Player 2 Hand</h3>
+              {hand2Data ? <Player handData={hand2Data} /> : <></>}
+            </div>
           </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 }
