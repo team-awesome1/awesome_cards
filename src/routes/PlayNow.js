d3aedc42e1d92683ee279274149c57b078de48d5
Patrick Sontag <pmsontag84@gmail.com>
2022-11-07 15:08:37 -0600

    set winner for hand

--- src/routes/PlayNow.js
+++ src/routes/PlayNow.js
@@ -1,6 +1,6 @@
 import Deck from "../deckData";
 import CardArea from "../CardArea";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Scoreboard from "../scoreboard/Scoreboard";
 import "../PlayNow.css";
 
@@ -9,7 +9,27 @@ export default function PlayNow() {
   const [hand2, setHand2] = useState(null);
   const [player1Card, setPlayer1Card] = useState(null);
   const [player2Card, setPlayer2Card] = useState(null);
-  const [gameStart, setGameStart] = useState(false)
+  const [winner, setWinner] = useState(null);
+  const [gameStart, setGameStart] = useState(false);
+  // useEffect(() => {
+  //   setWinner(compareCards(player1Card, player2Card));
+  // }, [player1Card, player2Card]);
+
+  const cardStrength = {
+    "2": 1,
+    "3": 2,
+    "4": 3,
+    "5": 4,
+    "6": 5,
+    "7": 6,
+    "8": 7,
+    "9": 8,
+    "0": 9,
+    "J": 10,
+    "Q": 11,
+    "K": 12,
+    "A": 13
+  }
 
   // function startGame() {
   //   const deck = new Deck();
@@ -42,10 +62,33 @@ export default function PlayNow() {
 
   function flipCard() {
     // cardsToWinner()
+    console.log("player1 hand", hand1)
+    console.log("player2 hand", hand2)
+    let card1 = hand1[0];
+    let card2 = hand2[0];
     setPlayer1Card(hand1.find((c, i) => i === 0));
     setPlayer2Card(hand2.find((c, i) => i === 0));
     setHand1(hand1.filter((c, i) => i !== 0));
     setHand2(hand2.filter((c, i) => i !== 0));
+
+    // compare flipped cards to find winner
+    console.log("player 1 and 2 cards:", player1Card, player2Card);
+    let bestCard = compareCards(card1, card2);
+    setWinner(bestCard);
+  }
+
+  function compareCards(cardCode1, cardCode2) {
+    let card1Num = cardCode1[0];
+    let card2Num = cardCode2[0];
+    let card1Value = cardStrength[card1Num];
+    let card2Value = cardStrength[card2Num];
+
+    if (card1Value === card2Value) return "tie";
+    if (card1Value > card2Value) {
+      return"player1";
+    } else {
+      return "player2";
+    }
   }
 
   // const cardsToWinner = (winner=1) => {
@@ -63,6 +106,7 @@ export default function PlayNow() {
     console.log("hand2", hand2);
     console.log("player1Card", player1Card);
     console.log("player2Card", player2Card);
+    console.log("winner", winner);
   }
 
   return (
